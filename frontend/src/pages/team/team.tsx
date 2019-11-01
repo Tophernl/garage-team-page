2ceb118c0eaaa0ef4487115c1f899b48902d4d47
Christopher Freudenberg <christopher.freudenberg@luminis.eu>
2019-11-01 15:17:36 +0100

    Get team-member data, save in state and show team member names

--- frontend/src/pages/team/team.tsx
+++ frontend/src/pages/team/team.tsx
@@ -1,11 +1,65 @@
 import React, { Component } from "react";
 import { Header } from "../../components/header/header";
+import { TeamMember } from "../../config/types";
+import { API_URL } from "../../config/api";
+import { Locations } from "../../config/enums";
 
-export class Team extends Component {
+type TeamState = {
+  teamMembers: TeamMember[];
+  locations: string[];
+  isLoading: boolean;
+  error: null | Error;
+};
+
+export class Team extends Component<{}, TeamState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      teamMembers: [],
+      locations: [],
+      isLoading: false,
+      error: null
+    };
+  }
+
+  componentDidMount(): void {
+    this.getTeamMembers();
+    this.getLocations();
+  }
+
+  getTeamMembers = () => {
+    this.setState({ isLoading: true });
+
+    fetch(API_URL.GET_TEAM_MEMBERS)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Uh oh! Could not read team data, please try again ..."
+          );
+        }
+      })
+      .then(data => this.setState({ teamMembers: data, isLoading: false }))
+      .catch(error => this.setState({ error: error, isLoading: false }));
+  };
+
+  getLocations = () => {
+    const locations = Object.values(Locations);
+    this.setState({ locations: locations });
+  };
+
   render() {
+    const { teamMembers } = this.state;
+
     return (
       <div className="team">
         <Header title="Team" />
+
+        {teamMembers.map((teamMember: TeamMember, index: number) => (
+          <div key={`member-${index}`}>{teamMember.name}</div>
+        ))}
       </div>
     );
   }
